3f5fe432a9b280b91e8e1e1c496185e4a3ee67a9
JhamilCrispin <90231315+JhamilCrispin@users.noreply.github.com>
2022-11-11 22:13:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pelimax/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.pelimax;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.pelimax.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.promocion:
+                    replaceFragment(new PromocionFragment());
+                    break;
+                case R.id.regional:
+                    replaceFragment(new RegionalFragment());
+                    break;
+                case R.id.carrito:
+                    replaceFragment(new ShopFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+
+    }
+}
